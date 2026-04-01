062cc567cd9c304fff6f31fb0472c8151c81208f
leojr74 <leonardo.jlpj@gmail.com>
2026-04-01 20:35:29 -0300

    Atualizar o 04_📑_Transações.py

--- finance_app/pages/04_📑_Transações.py
+++ finance_app/pages/04_📑_Transações.py
@@ -6,6 +6,7 @@ from categorizer import clean_description, find_category
 from ui import apply_global_style
 from sqlalchemy import text
 from utils.finance_tools import gerar_projeções_parcelas
+from utils.auth import check_login
 
 st.set_page_config(
     page_title="Gerenciamento de Transações",
@@ -13,22 +14,7 @@ st.set_page_config(
     layout="wide"
 )
 
-# 🔥 RESTAURA SESSÃO VIA URL (igual Home)
-if not st.session_state.get("logged_in"):
-    user_url = st.query_params.get("user")
 
-    if user_url:
-        st.session_state["logged_in"] = True
-        st.session_state["user"] = user_url
-        st.session_state["user_name"] = user_url  # fallback
-
-
-# 🔐 PROTEÇÃO DE PÁGINA
-if not st.session_state.get("logged_in"):
-    st.warning("Faça login para continuar")
-    st.stop()
-
-usuario_atual = st.session_state["user"]
 
 apply_global_style()
 
