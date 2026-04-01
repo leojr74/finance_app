90d3bfba3ac0758a2113bd6f4bf1a6430341d66b
leojr74 <leonardo.jlpj@gmail.com>
2026-04-01 13:35:04 -0300

    fix auth

--- finance_app/utils/auth.py
+++ finance_app/utils/auth.py
@@ -2,9 +2,14 @@ def check_login():
     import streamlit as st
 
     if not st.session_state.get("logged_in"):
+
         user_url = st.query_params.get("user")
 
+        # 🔥 CORREÇÃO CRÍTICA
         if user_url:
+            if isinstance(user_url, list):
+                user_url = user_url[0]
+
             st.session_state["logged_in"] = True
             st.session_state["user"] = user_url
             st.session_state["user_name"] = user_url
